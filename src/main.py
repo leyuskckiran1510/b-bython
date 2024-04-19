0d1a7ecd7006b81eff918cad7446dc988fd6c927
leyuskckiran1510 <kdhakal1510@gmail.com>
2024-04-19 10:01:36 +0545

    feat: add code block handeling

--- src/main.py
+++ src/main.py
@@ -16,6 +16,8 @@ INDENT_ABLES = [
     "case",
 ]
 
+SYMBOLS = ["{", "}", "(", ")", "[", "]"]
+
 
 def do_fstring(code: str, index: int):
     return index
@@ -45,8 +47,61 @@ def handel_quote(code: str, index: int):
     return index
 
 
+def do_open_braces(code: str, pointer: int):
+    line = ""
+    while code[pointer] != "{":
+        line += code[pointer]
+        pointer += 1
+    return line + ":", pointer
+
+
+def remove_tailing_close_braces(inner_chunk: str):
+    code_len = len(inner_chunk)
+    while code_len > 0:
+        code_len -= 1
+        if inner_chunk[code_len] != "}":
+            continue
+        else:
+            break
+    return inner_chunk[:code_len]
+
+
+def do_inner_chunk(code: str, pointer: int):
+    # back_up_pointer = pointer
+
+    symbol_stack = [code[pointer]]
+    pointer += 1
+    inner_chunk = ""
+    while symbol_stack:
+        char = code[pointer]
+        match char:
+            case x if x == "}" and symbol_stack[-1] == "{":
+                symbol_stack.pop()
+            case x if x == "]" and symbol_stack[-1] == "[":
+                symbol_stack.pop()
+            case x if x == ")" and symbol_stack[-1] == "(":
+                symbol_stack.pop()
+            case x if x in SYMBOLS:
+                symbol_stack.append(char)
+            case _:
+                ...
+        inner_chunk += char
+        pointer += 1
+    inner_chunk = remove_tailing_close_braces(inner_chunk)
+
+    inner_chunk = str(tokenizer(inner_chunk))
+    return inner_chunk, pointer + 1
+    # return "", back_up_pointer + 1
+
+
 def handel_word(code: str, cur_word: str, pointer: int):
-    return cur_word + code[pointer], pointer + 1
+    if cur_word in INDENT_ABLES:
+        until_open_braces, pointer = do_open_braces(code, pointer)
+        code_line = cur_word + until_open_braces
+        inner_chunk, pointer = do_inner_chunk(code, pointer)
+        return code_line + inner_chunk, pointer
+    else:
+        return cur_word + code[pointer], pointer + 1
 
 
 def tokenizer(raw_code: str):
