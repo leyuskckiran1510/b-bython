284d2dec03667d348376992c77ab89b6c594138f
leyuskckiran1510 <kdhakal1510@gmail.com>
2024-04-21 06:50:27 +0545

    feat: add support for f-string and semi-colen

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from util import load_sample
+from util import print_code  # type:ignore
 
 INDENT_ABLES = [
     "def",
@@ -16,10 +17,22 @@ INDENT_ABLES = [
     "case",
 ]
 
+INDENT_WITH = "    "
+
+# Normal Symbol stack
 SYMBOLS = ["{", "}", "(", ")", "[", "]"]
+# Symbol Stack for f-strings
+SYMBOLS_F = ["{", "}", "(", ")", "[", "]", '"', "'"]
 
 
 def do_fstring(code: str, index: int):
+    stack_symbol = [code[index]]
+    index += 1
+    while index < len(code) and stack_symbol:
+        char = code[index]
+        if char in SYMBOLS_F:
+            symbol_stack_op(stack_symbol, char, SYMBOLS_F)
+        index += 1
     return index
 
 
@@ -42,17 +55,51 @@ def handel_quote(code: str, index: int):
             index += 1
             while code[index] != quote:
                 index += 1
-
             index += 1
     return index
 
 
+def symbol_stack_op(
+    sym_stack: list[str],
+    cur_sym: str,
+    symbols: list[str] = SYMBOLS,
+):
+    match cur_sym:
+        # remove the epsilon from the stack
+        case x if x == "{" and sym_stack[-1] == "e":
+            sym_stack.pop()
+        case x if x == "}" and sym_stack[-1] == "{":
+            sym_stack.pop()
+        case x if x == "]" and sym_stack[-1] == "[":
+            sym_stack.pop()
+        case x if x == ")" and sym_stack[-1] == "(":
+            sym_stack.pop()
+        case x if x == '"' and sym_stack[-1] == '"':
+            sym_stack.pop()
+        case x if x == "'" and sym_stack[-1] == "'":
+            sym_stack.pop()
+        case x if x in symbols:
+            sym_stack.append(cur_sym)
+        case _:
+            ...
+
+
 def do_open_braces(code: str, pointer: int):
+    # Accept by empty Stack
     line = ""
-    while code[pointer] != "{":
-        line += code[pointer]
+    # insert epsilon  as star symbol
+    symbol_stack: list[str] = ["e"]
+    while symbol_stack and len(code) > pointer:
+        char = code[pointer]
+        match char:
+            case x if x in SYMBOLS:
+                symbol_stack_op(symbol_stack, char)
+            case _:
+                ...
+        if char != "\n" and symbol_stack:
+            line += char
         pointer += 1
-    return line + ":", pointer
+    return line + ":\n", pointer - 1
 
 
 def remove_tailing_close_braces(inner_chunk: str):
@@ -66,45 +113,55 @@ def remove_tailing_close_braces(inner_chunk: str):
     return inner_chunk[:code_len]
 
 
-def do_inner_chunk(code: str, pointer: int):
-    # back_up_pointer = pointer
+def indent_it(code_block: str, level: int):
+    new_code = INDENT_WITH * level
+    pointer = 0
+    while pointer < len(code_block):
+        char = code_block[pointer]
+        if char == "\n":
+            char = "\n" + INDENT_WITH * level
+        new_code += char
+        pointer += 1
+    return new_code
 
+
+def do_inner_chunk(code: str, pointer: int, level: int = 0):
     symbol_stack = [code[pointer]]
     pointer += 1
-    inner_chunk = ""
-    while symbol_stack:
+    inner_chunk: str = ""
+    while symbol_stack and len(code) > pointer:
         char = code[pointer]
         match char:
-            case x if x == "}" and symbol_stack[-1] == "{":
-                symbol_stack.pop()
-            case x if x == "]" and symbol_stack[-1] == "[":
-                symbol_stack.pop()
-            case x if x == ")" and symbol_stack[-1] == "(":
-                symbol_stack.pop()
             case x if x in SYMBOLS:
-                symbol_stack.append(char)
+                symbol_stack_op(symbol_stack, char)
             case _:
                 ...
+        if char == ";" and symbol_stack == ["{"]:
+            while char == ";":
+                char = code[pointer]
+                pointer += 1
+            pointer -= 2
+            char = "\n"
         inner_chunk += char
         pointer += 1
     inner_chunk = remove_tailing_close_braces(inner_chunk)
+    inner_chunk = tokenizer(inner_chunk, level=level)
+    inner_chunk = indent_it(inner_chunk, level)
+    # print_code(inner_chunk, level)
+    return inner_chunk, pointer
 
-    inner_chunk = str(tokenizer(inner_chunk))
-    return inner_chunk, pointer + 1
-    # return "", back_up_pointer + 1
 
-
-def handel_word(code: str, cur_word: str, pointer: int):
+def handel_word(code: str, cur_word: str, pointer: int, level: int = 0):
     if cur_word in INDENT_ABLES:
         until_open_braces, pointer = do_open_braces(code, pointer)
         code_line = cur_word + until_open_braces
-        inner_chunk, pointer = do_inner_chunk(code, pointer)
+        inner_chunk, pointer = do_inner_chunk(code, pointer, level)
         return code_line + inner_chunk, pointer
     else:
         return cur_word + code[pointer], pointer + 1
 
 
-def tokenizer(raw_code: str):
+def tokenizer(raw_code: str, level: int = 0):
     new_code: str = ""
     cur_word = ""
     pointer = 0
@@ -114,20 +171,27 @@ def tokenizer(raw_code: str):
         match char:
             case x if x in ['"', "'"]:
                 n_pointer = handel_quote(raw_code, pointer)
-                new_code += raw_code[pointer:n_pointer]
+                cur_word += raw_code[pointer:n_pointer]
                 pointer = n_pointer
             case x if x in [" ", "\n"]:
-                new_chunk, pointer = handel_word(raw_code, cur_word, pointer)
+                new_chunk, pointer = handel_word(
+                    raw_code, cur_word, pointer, level + 1
+                )
                 new_code += new_chunk
                 cur_word = ""
             case _:
                 cur_word += char
                 pointer += 1
+        # print_code(new_code, level)
     return new_code + cur_word
 
 
 if __name__ == "__main__":
-    for file, sample in load_sample(1):
+    for file, sample in load_sample(3):
         print(f"Original FIle {file}..")
         print("Tokonized into... ")
-        print(tokenizer(sample))
+        python_code = tokenizer(sample, 0)
+        # with open("output.py", "w") as fp:
+        #     fp.write(python_code)
+        # exec(python_code)
+        print(python_code)
--- src/util.py
+++ src/util.py
@@ -10,3 +10,14 @@ def load_sample(file_index: int):
     for file in files:
         with open(str(file), "r") as fp:
             yield file, fp.read()
+
+
+def print_code(new_code: str, level: int):
+    if not new_code:
+        return
+    print("=" * 12)
+    print(f"Indendation Level {level=}")
+    print("=" * 12)
+    print(new_code)
+    print("=" * 12)
+    print("\n")
